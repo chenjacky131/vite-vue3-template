da9222cc17543f3c44e0500c416d55559a03da41
ccw <chencw@cniship.com>
2022-06-22 16:51:06 +0800

    增加一些配置

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// console.log(import.meta.env)  //读取配置文件
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      scss: {
+        // 给导入的路径最后加上 ;
+        additionalData: '@import "./src/assets/variable.scss";'
+      }
+    }
+  },
+  resolve: {
+    '@': path.resolve(__dirname, './src')
+  }
 })
