7dc6c07cf2f318c1a3a68bb7e0ab1aa6a6e5a675
ccw <chenjacky131@gmail.com>
2022-10-24 17:55:36 +0800

    feat
    配置404页面；

--- src/router/index.js
+++ src/router/index.js
@@ -13,14 +13,24 @@ const router = createRouter({
       name: 'hello',
       component: () => import('@/components/HelloWorld.vue')
     },
+    {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/components/NotFound.vue')
+    },
   ],
 });
-router.beforeEach(async (to, from) => {
+router.beforeEach(async (to, from, next) => {
   /*
     1. return false  //  返回false取消导航
     2. return { name: 'Login' } // 将用户重定向到登录页面
   */
   NProgress.start();  //  开始加载进度条
+  // if(to.name === 'hello'){  //  路由拦截
+  //   next({name: 'home'})
+  // }else{
+  //   next()
+  // }
+  next()
 });
 router.afterEach((to, from) => {
   NProgress.done(); //  结束加载进度条
