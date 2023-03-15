206dcb456595ad0146c8cb8cc2b92035318312b4
ccw <chencw@cniship.com>
2023-03-15 10:00:45 +0800

    配置图片压缩插件

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,8 +1,10 @@
 <script setup>
+import Logo from '@/assets/logo.png'
 </script>
 
 <template>
   <h1>jacky131-cli</h1>
+  <img :src="Logo"/>
   <router-link to="/hello-world">Hello World</router-link>
 </template>
 
